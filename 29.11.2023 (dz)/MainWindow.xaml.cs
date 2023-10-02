3d87fd7e38846f8fd55ff28a264ab89c64aa9f9b
Sele_tu30@student.itstep.org <Sele_tu30@student.itstep.org>
2023-10-02 17:10:04 +0300

    1

--- 29.11.2023 (dz)/MainWindow.xaml.cs
+++ 29.11.2023 (dz)/MainWindow.xaml.cs
@@ -25,6 +25,8 @@ namespace _29._11._2023__dz_
         public MainWindow()
         {
             InitializeComponent();
+
+            headerTemplateTabitems = CreateTabHeaderCrosImageTemplate();
         }
 
         private void Button_Minimaze_Click(object sender, RoutedEventArgs e)
@@ -42,19 +44,87 @@ namespace _29._11._2023__dz_
             Close();
         }
 
-        private void TabItem_MouseClick(object sender, MouseButtonEventArgs e)
+        public DataTemplate CreateTabHeaderCrosImageTemplate()
+        {
+            DataTemplate dataTemplate = new DataTemplate();
+
+            FrameworkElementFactory stackPanelFactory = new FrameworkElementFactory(typeof(StackPanel));
+            stackPanelFactory.SetValue(StackPanel.OrientationProperty, Orientation.Horizontal);
+
+            FrameworkElementFactory textBlockFactory = new FrameworkElementFactory(typeof(TextBlock));
+            textBlockFactory.SetBinding(TextBlock.TextProperty, new System.Windows.Data.Binding());
+            textBlockFactory.SetValue(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center);
+
+            FrameworkElementFactory buttonFactory = new FrameworkElementFactory(typeof(Button));
+            SolidColorBrush buttonBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4E5083"));
+            buttonFactory.SetValue(Button.BackgroundProperty, buttonBackground);
+            buttonFactory.SetValue(Button.PaddingProperty, new Thickness(5));
+            buttonFactory.SetValue(Button.MarginProperty, new Thickness(2));
+            buttonFactory.SetValue(Button.BorderThicknessProperty, new Thickness(0));
+            buttonFactory.AddHandler(Button.ClickEvent, new RoutedEventHandler(CloseImage));
+
+            FrameworkElementFactory imageFactory = new FrameworkElementFactory(typeof(Image));
+            Uri uri = new Uri("pack://application:,,,/29.11.2023 (dz);component/cross.png", UriKind.Absolute);
+            imageFactory.SetValue(Image.SourceProperty, new BitmapImage(uri));
+
+            imageFactory.SetValue(Image.WidthProperty, 15.0);
+            imageFactory.SetValue(Image.HeightProperty, 15.0);
+
+            buttonFactory.AppendChild(imageFactory);
+            stackPanelFactory.AppendChild(textBlockFactory);
+            stackPanelFactory.AppendChild(buttonFactory);
+
+            dataTemplate.VisualTree = stackPanelFactory;
+            return dataTemplate;
+
+        }
+
+        private void OneNewImage(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "*.PNG (*.png)|*.png|*.JPG (*.jpg)|*.jpg|Все файлы (*.*)|*.*"; 
-            openFileDialog.InitialDirectory = @"C:\"; 
+            openFileDialog.Filter = "*.PNG (*.png)|*.png|*.JPG (*.jpg)|*.jpg|Все файлы (*.*)|*.*";
+            openFileDialog.InitialDirectory = @"C:\";
 
-            bool? result = openFileDialog.ShowDialog(); 
+            bool? result = openFileDialog.ShowDialog();
 
             if (result == true)
             {
                 tabControl.Items.Add(CreateTabItemWithContent(openFileDialog.FileName));
             }
         }
+
+        private void CloseImage(object sender, RoutedEventArgs e)
+        {
+            TabItem tabItem = FindParent<TabItem>(sender as Button);
+
+            if (tabItem != null) 
+            {
+                if (tabControl.SelectedIndex == tabControl.Items.IndexOf(tabItem))
+                {
+                    tabControl.SelectedIndex = 0;
+                }
+                tabControl.Items.Remove(tabItem); 
+            }
+        }
+
+        public static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            if (parentObject == null) return null;
+
+            T parent = parentObject as T;
+            if (parent != null)
+            {
+                return parent;
+            }
+            else
+            {
+                return FindParent<T>(parentObject);
+            }
+        }
+
+        DataTemplate headerTemplateTabitems = null;
 
         public TabItem CreateTabItemWithContent(string imagePath)
         {
@@ -63,6 +133,7 @@ namespace _29._11._2023__dz_
 
             Grid grid = new Grid();
             grid.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#6B6793"));
+            
 
             Image image = new Image();
             image.Source = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
@@ -84,6 +155,7 @@ namespace _29._11._2023__dz_
             grid.Children.Add(image);
 
             tabItem.Content = grid;
+            tabItem.HeaderTemplate = headerTemplateTabitems;
 
             return tabItem;
         }
@@ -114,6 +186,9 @@ namespace _29._11._2023__dz_
             scaleTransform.ScaleX = Math.Max(0.1, scaleTransform.ScaleX);
             scaleTransform.ScaleY = Math.Max(0.1, scaleTransform.ScaleY);
 
+            Point currentPoint = e.GetPosition(this);
+            scaleTransform.CenterX = currentPoint.X;
+            scaleTransform.CenterY = currentPoint.Y;
         }
 
         private bool isDragging = false;
@@ -126,6 +201,8 @@ namespace _29._11._2023__dz_
             {
                 isDragging = true;
                 startPoint = e.GetPosition(this);
+                startTranslate.X = translateTransform.X;
+                startTranslate.Y = translateTransform.Y;
                 image.CaptureMouse();
             }
         }
@@ -142,13 +219,6 @@ namespace _29._11._2023__dz_
                 translateTransform.X = startTranslate.X + deltaX;
                 translateTransform.Y = startTranslate.Y + deltaY;
             }
-            else
-            {
-                startPoint = currentPoint;
-                startTranslate.X = translateTransform.X;
-                startTranslate.Y = translateTransform.Y;
-            }
-
         }
 
         private void Image_MouseUp(object sender, MouseButtonEventArgs e)
@@ -157,6 +227,17 @@ namespace _29._11._2023__dz_
             {
                 isDragging = false;
                 image.ReleaseMouseCapture();
+            }
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) 
+            {
+                scaleTransform.ScaleX = 1;
+                scaleTransform.ScaleY = 1;
+                translateTransform.X = 0;
+                translateTransform.Y = 0;
             }
         }
     }
