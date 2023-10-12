43192b21c39242253d460d825f8fd27d8405e35a
TheSheleng <128699288+TheSheleng@users.noreply.github.com>
2023-10-12 21:09:14 +0300

    6.10.2023

---
+++ 6.10.2023/CreditProperty.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CreditProperty
+{
+    class CreditProperty : FrameworkElement
+    {
+        static FrameworkPropertyMetadata Metadata;
+
+        public static readonly DependencyProperty DataProperty;
+
+        static CreditProperty()
+        {
+            Metadata = new FrameworkPropertyMetadata(
+                    new PropertyChangedCallback(ChangedCallbackMethod), 
+                    new CoerceValueCallback(CoerceValueCallBackMethod)
+                );
+
+            DataProperty = DependencyProperty.Register(
+                    "Data",
+                    typeof(double),
+                    typeof(CreditProperty),
+                    Metadata
+                );
+        }
+
+        public double Data 
+        {
+            get { return (double)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        static object CoerceValueCallBackMethod(DependencyObject d, object value)
+        {
+            return (double)value * 12 * 0.36;
+        }
+
+        static void ChangedCallbackMethod(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Application.Current.MainWindow.Title = e.NewValue.ToString();
+        }
+    }
+}
